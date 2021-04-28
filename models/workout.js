3e369349b9246cf5ae1c512a44a968ab24f1eaee
lpenen <75550501+lpenenburgh@users.noreply.github.com>
2021-04-28 15:55:41 -0600

    schema content

--- models/workout.js
+++ models/workout.js
@@ -7,15 +7,35 @@ const WorkoutSchema = new Schema ({
         type: Date,
         default: Date.now
     },
-    //exercise
-        //type
-        //name
-        //duration
-        //weight
-        //reps
-        //sets
-        //distance
-})
+    exercises: [
+        {
+            type: {
+                type: String,
+                required: true,
+            },
+            name: {
+                type: String,
+                required: true, 
+            },
+            duration: {
+                type: Number,
+                required: true,
+            },
+            weight: {
+                type: Number,
+            },
+            reps: {
+                type: Number,
+            },
+            sets: {
+                type: Number,
+            },
+            distance: {
+                type: Number,
+            },
+        },
+    ],
+});
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
