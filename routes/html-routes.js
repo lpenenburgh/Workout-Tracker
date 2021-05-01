9b136390a2e0916c1d795a51cb63996a5088fb8b
lpenen <75550501+lpenenburgh@users.noreply.github.com>
2021-05-01 16:08:56 -0600

    finished html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,7 +2,13 @@ const router = require("express").Router();
 const path = require("path");
 
 //route to exercise page
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
 
 //route to stats page
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
 
 module.exports = router
