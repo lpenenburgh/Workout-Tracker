799f3dcced7236d09d3e06fe9305fa719dc7cf08
lpenen <75550501+lpenenburgh@users.noreply.github.com>
2021-05-03 12:07:36 -0600

    apiroute structure

---
+++ routes/api-routes.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const Workout = require("../models/Workout");
+
+//get all workouts and their duration
+
+
+//get workouts in range(using aggregate)
+
+
+//New workout
+
+
+//New exercise
+
+
+module.exports = router;
