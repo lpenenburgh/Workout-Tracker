7daf09886fe0cad4d489838d1a71d4f6fbd726cb
lpenen <75550501+lpenenburgh@users.noreply.github.com>
2021-05-03 12:17:44 -0600

    get all workouts route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,8 +1,17 @@
 const router = require("express").Router();
+//does this need .js at the end?
 const Workout = require("../models/Workout");
 
 //get all workouts and their duration
-
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout)
+        })
+        .catch(err => {
+            res.json(err)
+        });
+});
 
 //get workouts in range(using aggregate)
 
